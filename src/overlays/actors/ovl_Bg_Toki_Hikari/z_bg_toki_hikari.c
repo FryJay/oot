e33325046dc8296ea5ba70c47e7020533c046ac2
FryJay <darin.j.fry@gmail.com>
2020-09-26 23:03:59 -0700

    Initial work

--- src/overlays/actors/ovl_Bg_Toki_Hikari/z_bg_toki_hikari.c
+++ src/overlays/actors/ovl_Bg_Toki_Hikari/z_bg_toki_hikari.c
@@ -9,7 +9,9 @@ void BgTokiHikari_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void BgTokiHikari_Update(Actor* thisx, GlobalContext* globalCtx);
 void BgTokiHikari_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+void func_808B9F98(BgTokiHikari* this, GlobalContext* globalCtx);
+void func_808BA204(BgTokiHikari* this, GlobalContext* globalCtx);
+
 const ActorInit Bg_Toki_Hikari_InitVars = {
     ACTOR_BG_TOKI_HIKARI,
     ACTORTYPE_BG,
@@ -21,8 +23,31 @@ const ActorInit Bg_Toki_Hikari_InitVars = {
     (ActorFunc)BgTokiHikari_Update,
     (ActorFunc)BgTokiHikari_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Init.s")
+
+static s32 D_808BAC70 = { 0x485003E8, 0x00000000, 0x00000000, 0x00000000 };
+
+void BgTokiHikari_Init(Actor *thisx, GlobalContext *globalCtx) {
+    BgTokiHikari *this = THIS;
+    s16 temp_v0;
+
+    temp_v0 = thisx->params;
+    if (temp_v0 == 0) {
+        Actor_ProcessInitChain(thisx, (InitChainEntry *) &D_808BAC70);
+        this->actionFunc = func_808B9F98;
+        return;
+    }
+    if (temp_v0 != 1) {
+        return;
+    }
+    if ((*(&gSaveContext + 0xEDC) & 0x800) == 0) {
+        thisx->actionFunc = func_808BA204;
+        thisx->unk_14C = 0.0f;
+        return;
+    }
+    Actor_Kill(thisx);
+}
+
+// #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Init.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_Bg_Toki_Hikari/BgTokiHikari_Destroy.s")
 
